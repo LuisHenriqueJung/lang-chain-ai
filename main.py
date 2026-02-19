7bebdbe9b9704fdda4edc49f4889c585c85fb589
ejun1074 <luis@rsdata.inf.br>
2026-02-18 22:20:44 -0300

    Entendendo a estrutura inicial

---
+++ main.py
@@ -0,0 +1,17 @@
+from langchain_openai import ChatOpenAI
+from langchain_core.messages import SystemMessage, HumanMessage, AIMessage
+from dotenv import load_dotenv
+
+load_dotenv()
+
+messages = [
+    SystemMessage(content="You are a helpful assistant.No máximo 140 caracteres.")
+    ]
+
+model = ChatOpenAI()
+
+prompt = input("Enter your prompt: ")
+messages.append(HumanMessage(content=prompt))
+response = model.invoke(messages, max_tokens=140, temperature=0.7, stop=["\n"])
+print(response.content)
+print(response.usage_metadata)
